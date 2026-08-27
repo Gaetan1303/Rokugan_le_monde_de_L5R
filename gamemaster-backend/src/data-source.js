ae545846468e9c144fdfef6b3591b4340b3996d7
Gaetan1303 <gaetan.begue13@gmail.com>
2026-08-27 16:32:39 +0200

    yarn patch et correction de petit bugs

--- gamemaster-backend/src/data-source.js
+++ gamemaster-backend/src/data-source.js
@@ -1,20 +1,46 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppDataSource = void 0;
-require("dotenv/config");
-var typeorm_1 = require("typeorm");
-var User_js_1 = require("./models/User.js");
-var Scene_js_1 = require("./models/Scene.js");
-var Scenario_js_1 = require("./models/Scenario.js");
-var databaseUrl = process.env.DATABASE_URL;
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+import { User } from './models/User.js';
+import { Scene } from './models/Scene.js';
+import { Scenario } from './models/Scenario.js';
+import { Room } from './models/Room.js';
+import { PlayerInRoom } from './models/PlayerInRoom.js';
+
+const databaseUrl = process.env.DATABASE_URL;
+
 if (!databaseUrl) {
-    throw new Error('DATABASE_URL n\'est pas défini dans les variables d\'environnement.');
+  throw new Error(
+    "DATABASE_URL n'est pas défini dans les variables d'environnement."
+  );
 }
-exports.AppDataSource = new typeorm_1.DataSource({
-    type: "postgres",
-    url: databaseUrl,
-    synchronize: true,
-    logging: false,
-    entities: [User_js_1.User, Scene_js_1.Scene, Scenario_js_1.Scenario],
-    ssl: { rejectUnauthorized: false }
+
+const isProd =
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'prod';
+
+const isRenderInternal =
+  databaseUrl.includes('.internal') ||
+  databaseUrl.includes('dpg-');
+
+const entities = isProd
+  ? ['dist/models/**/*.js', 'dist/models/*.js']
+  : [User, Scene, Scenario, Room, PlayerInRoom];
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: databaseUrl,
+
+  entities,
+
+  synchronize: true,
+  logging: false,
+
+  ssl: isRenderInternal
+    ? false
+    : {
+        rejectUnauthorized: false
+      },
+
+  connectTimeoutMS: 10000
 });
